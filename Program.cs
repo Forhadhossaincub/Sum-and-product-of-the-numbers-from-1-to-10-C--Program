1d387aa0255171ba1d53f1174d20f32416cfd6cf
FORHAD HOSSAIN <forhadhossaincub@gmail.com>
2022-08-13 22:51:56 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_and_product_of_the_numbers_from_1_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int prod;
+            int sum;
+            int i;
+            sum = 0;
+            prod = 1;
+            for (i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                prod = prod * i;
+
+            }
+
+
+            Console.WriteLine("Sum is " + sum);
+            Console.WriteLine("Product is " + prod);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Sum-and-product-of-the-numbers-from-1-to-10
+ Sum-and-product-of-the-numbers-from-1-to-10
